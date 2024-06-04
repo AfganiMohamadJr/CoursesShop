b53e34894823c70ef7fb4f2ac5e670cbe6746866
MohamadAfJr <Afgani11@mail.ru>
2024-06-04 14:37:02 +0300

    Create Table in DataBase

---
+++ shop/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    prise = models.FloatField()
+    student_qty = models.IntegerField()
+    reviews_qty = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
