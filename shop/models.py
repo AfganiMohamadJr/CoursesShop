2a873f06f0430bd9a7d24b7cba6bcfaff50675d1
MohamadAfJr <Afgani11@mail.ru>
2024-06-05 00:10:36 +0300

    Add to  Table

--- shop/models.py
+++ shop/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
     
 class Course(models.Model):
     title = models.CharField(max_length=100)
     prise = models.FloatField()
     student_qty = models.IntegerField()
-    reviews_qty = models.ImageField()
+    reviews_qty = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
